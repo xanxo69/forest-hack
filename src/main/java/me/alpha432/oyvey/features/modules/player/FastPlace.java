a5fddc7e404d23b65cf2cb2a5ee8c1c1be480fe5
xanxo69 <jamesshearer100@gmail.com>
2025-07-05 11:06:20 +0100

    Update FastPlace.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Items;
 
 public class FastPlace extends Module {
     public FastPlace() {
-        super("FastPlace", "Makes you throw exp faster", Category.PLAYER, true, false, false);
+        super("superplace", "Makes you throw exp faster", Category.PLAYER, true, false, false);
     }
 
     @Override public void onUpdate() {
